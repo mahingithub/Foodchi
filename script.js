e66388cdecb4d18aa3c7b6fb8be4ac9009a845d1
mahingithub <139889810+mahingithub@users.noreply.github.com>
2024-08-21 23:45:48 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+var typed = new Typed(".multiple-text", {
+                                        strings: ["Food","Yummy Food!","🍔","🍟","🍨","🍧","🎂","🍪","🌭","🍕","🍙"],
+                                        typeSpeed: 100,
+                                        backSpeed: 100,
+                                        backDelay: 1000,
+                                        loop: true,
+                                        });
+                                    
+                                    
